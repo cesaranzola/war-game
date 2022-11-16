da2bf89eda912030e941da787c437d8bb049d445
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-11-16 14:58:40 -0500

    Add basic style

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-const deckId = [];
+let deckId;
+const imgContainer = document.getElementById('images');
 
 function handleClick() {
 	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
 		.then((res) => res.json())
-		.then((data) => deckId.push(data.deck_id));
+		.then((data) => (deckId = data.deck_id));
+}
+
+function render(data) {
+	let html = ``;
+	for (let item of data.cards) {
+		html += `<div class="cardx"><img src="${item.image}" alt="Card" class="img-fluid" /></div>`;
+	}
+	imgContainer.innerHTML = html;
+}
+
+function drawCard() {
+	if (!deckId) {
+		handleClick();
+		setTimeout(() => {
+			fetch(
+				`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
+			)
+				.then((res) => res.json())
+				.then((data) => {
+					render(data);
+				});
+		}, 500);
+	}
 }
 
 document.getElementById('mainbtn').addEventListener('click', handleClick);
-
-console.log(deckId);
+document.getElementById('draw').addEventListener('click', drawCard);
