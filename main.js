9517d67c8892162c7267551a09d5156d19adcbec
cesaranzola <cesar.rodriguez.anzola@protonmail.com>
2022-11-16 17:54:53 -0500

    Add function to determine the winner of each round

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let deckId;
 const imgContainer = document.getElementById('images');
+const mainBtn = document.getElementById('mainbtn');
+const draw = document.getElementById('draw');
 
 function handleClick() {
 	fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
@@ -13,22 +15,58 @@ function render(data) {
 		html += `<div class="cardx"><img src="${item.image}" alt="Card" class="img-fluid" /></div>`;
 	}
 	imgContainer.innerHTML = html;
+	setWinner(data);
 }
 
 function drawCard() {
 	if (!deckId) {
 		handleClick();
 		setTimeout(() => {
-			fetch(
-				`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
-			)
-				.then((res) => res.json())
-				.then((data) => {
-					render(data);
-				});
+			fetchCard();
 		}, 500);
+	} else {
+		fetchCard();
 	}
 }
 
-document.getElementById('mainbtn').addEventListener('click', handleClick);
-document.getElementById('draw').addEventListener('click', drawCard);
+function fetchCard() {
+	fetch(
+		`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
+	)
+		.then((res) => res.json())
+		.then((data) => {
+			render(data);
+		});
+}
+
+function setWinner(data) {
+	const cardsValues = [
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'10',
+		'JACK',
+		'QUEEN',
+		'KING',
+		'ACE',
+	];
+
+	const card1Index = cardsValues.indexOf(data.cards[0].value);
+	const card2Index = cardsValues.indexOf(data.cards[1].value);
+
+	if (card1Index > card2Index) {
+		console.log('The winner is the Computer!');
+	} else if (card2Index > card1Index) {
+		console.log("You're the winner!");
+	} else {
+		console.log("It's a tie!");
+	}
+}
+
+mainBtn.addEventListener('click', handleClick);
+draw.addEventListener('click', drawCard);
